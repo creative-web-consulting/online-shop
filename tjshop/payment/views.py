8931148f9b693a461e85d4a0f85b1dda05052181
FHJBC <bosco.hjf@gmail.com>
2022-07-17 16:30:50 +0300

    feature: sending PDF files by email

--- tjshop/payment/views.py
+++ tjshop/payment/views.py
@@ -2,6 +2,7 @@ import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 
 # instantiate Braintree payment gateway
@@ -34,6 +35,9 @@ def payment_process(request):
             order.braintree_id = result.transaction.id
 
             order.save()
+
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
             return redirect('payment:done')
         else:
